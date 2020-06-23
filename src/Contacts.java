aae784db8b7bd4f254a62c7de0849f8023452a2a
Greg Jett <greg.jett25@gmail.com>
2020-06-23 10:59:38 -0500

    java files

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,13 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Contacts {
-    private String contact;
+    public class Contacts {
+//    private String contact;
+    private String name;
+    private String number;
 
     // getters and setters for contacts
-    public Contacts(String contact){this.contact = contact;}
-    public String getContact(){ return contact;}
-    public void setContact(String contact){ this.contact = contact;}
+    public Contacts(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
+
+//    public String getContact(){ return contact;}
+//    public void setContact(String contact){ this.contact = contact;}
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public void setNumber(String number) {
+            this.number = number;
+        }
+
 
 //    public static List<String> contactList(List<Contacts> contacts){
 //        List<String> contacts = new ArrayList<>();
--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -1,49 +1,83 @@
-import util.Input;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class ContactsApplication {
-
-   static Input input = new Input();
-
-
-   // creating user interface
     public static void main(String[] args) {
-        boolean userChoice = true;
-        do{
-            System.out.println("1. View contacts.\n" +
-                    "2. Add a new contact\n" +
-                    "3. Search a contact by name\n" +
-                    "4. Delete an existing contact\n" +
-                    "5. Exit\n" +
-                    "Enter and option (1, 2, 3, 4, or 5)");
-            int userDecides = input.getInt(1,5);
-            input.getString();
-            if (userDecides == 1){
-
-            }
-        } while(userChoice);
 
 
+        String directory = "test";
+        Path myContacts = Paths.get(directory, "contacts.txt");
 
-        // working on File IO for contacts and creating the path to contacts.txt
-        Path myDirectory = Paths.get("src");
-        Path myContacts = Paths.get("src/util", "contacts.txt");
-        System.out.println(Files.exists(myDirectory));
+//        System.out.println(Files.exists(myDirectory));
         System.out.println(Files.exists(myContacts));
 
-        List<Contacts> contacts;
+
+
+//        String[] name = {"michael", "greg"};
+//        List<String> contacts = null;
+//        try {
+//            contacts = Files.readAllLines(myContacts);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        System.out.println(contacts);
+//        contacts.add("greg");
+
         List<String> contact = new ArrayList<>();
+        contact.add("Michael");
+        contact.add("Greg");
+
 
         try {
-            contact = Files.readAllLines(myDirectory);
-        } catch (IOException e){
+            Files.write(myContacts,contact);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        System.out.println(contacts);
     }
+
+//   static Input input = new Input();
+//
+//
+//   // creating user interface
+//    public static void main(String[] args) {
+//        boolean userChoice = true;
+//        do{
+//            System.out.println("1. View contacts.\n" +
+//                    "2. Add a new contact\n" +
+//                    "3. Search a contact by name\n" +
+//                    "4. Delete an existing contact\n" +
+//                    "5. Exit\n" +
+//                    "Enter and option (1, 2, 3, 4, or 5)");
+//            int userDecides = input.getInt(1,5);
+//            input.getString();
+//            if (userDecides == 1){
+//
+//            }
+//        } while(userChoice);
+//
+//
+//
+//        // working on File IO for contacts and creating the path to contacts.txt
+//        Path myDirectory = Paths.get("src");
+//        Path myContacts = Paths.get("src/util", "contacts.txt");
+//        System.out.println(Files.exists(myDirectory));
+//        System.out.println(Files.exists(myContacts));
+//
+//        List<Contacts> contacts;
+//        List<String> contact = new ArrayList<>();
+//
+//        try {
+//            contact = Files.readAllLines(myDirectory);
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
 }
